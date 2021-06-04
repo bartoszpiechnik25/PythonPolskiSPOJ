e4388d1803c248045f93ecfe72c9b2ab3b2e7dec
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2021-06-04 11:14:56 +0200

    Add files via upload

---
+++ FR_AA_08 - Dwa skoczki szachowe .py
@@ -0,0 +1,37 @@
+# Napisz program, który sprawdzi, czy dwa skoczki szachowe ustawione na szachownicy wzajemnie się atakują.
+# Wejście:
+# W pierwszym i jedynym wierszu wejścia znajdują się dwie różne współrzędne pól szachownicy z przedziału [a1 - h8]
+# , na których ustawione są dwa skoczki szachowe.
+# Wyjaśnienie:
+# Współrzędne definiowane są za pomocą kombinacji liter i liczb. Kolumny szachownicy oznaczone są małymi literami od a
+# do h, a wiersze ponumerowane są od 1 do 8.
+# Skoczek porusza się o jedno pole do przodu (w pionie lub poziomie), po czym o jedno pole na ukos w dowolnym kierunku.
+# Rysunek poniżej przedstawia wszystkie możliwe ruchy skoczka znajdującego się na polu d5.
+# Dwa skoczki szachowe atakują się wzajemnie, gdy wzajemnie znajdują się na polach, na które mogą wykonać ruch.
+# Wyjście:
+# Na wyjściu należy wypisać słowo TAK, jeśli skoczki szachowe wzajemnie się atakują. W przeciwnym przypadku należy
+# wypisać słowo NIE.
+
+from math import sqrt
+
+wsp = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+
+
+def knight_place(co_ordinates):
+    ans = []
+    if co_ordinates[0].lower() in wsp:
+        a = wsp[co_ordinates[0].lower()]
+        b = int(co_ordinates[1]) - 1
+        ans = [b, a]
+    return ans
+
+a, b = map(str, input().split())
+
+knight_1 = knight_place(co_ordinates=a)
+knight_2 = knight_place(co_ordinates=b)
+
+ans = sqrt((knight_1[0] - knight_2[0]) ** 2 + (knight_1[1] - knight_2[1]) ** 2)
+if ans == sqrt(5):
+    print("TAK")
+else:
+    print("NIE")
