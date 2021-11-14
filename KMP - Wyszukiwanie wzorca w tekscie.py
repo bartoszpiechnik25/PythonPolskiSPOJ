09604bdd24a0d5fdfca7c6b1196dcbf0c256ee9a
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2021-11-14 18:33:48 +0100

    Update KMP - Wyszukiwanie wzorca w tekscie.py

--- KMP - Wyszukiwanie wzorca w tekscie.py
+++ KMP - Wyszukiwanie wzorca w tekscie.py
@@ -1,13 +1,11 @@
 #  Napisz program, który dla danego wzorca P (ang. pattern) i tekstu T wypisze pozycje, na których znajduje się wzorzec
 #  P jako podsłowo tekstu T.
 # Wejście
-#
 # W pierwszym wierszu danych znajduje się liczba natralna T (0 < T < 11) oznaczająca liczbę zestawów danych.
 # Każdy zestaw danych podany jest w trzech wierszach. Pierwszy zawiera jedną liczbę naturalną n - oznaczającą długość
 # wzorca P ( n <= 1000000 ). Drugi wiersz zawiera wzorzec P - napis złożony z n liter angielskiego alfabetu (a-z, A-Z).
 # Trzeci wiersz zawiera test T czyli ciąg liter alfabetu angielskiego zakończony znakiem nowego wiersza.
 # Wyjście
-#
 # Dla każdego zestawu danych wypisz pozycje na których wzorzec P pasuje w tekscie T. Zakładamy, że litery tekstu T
 # numerowane są od zera.
 import re
